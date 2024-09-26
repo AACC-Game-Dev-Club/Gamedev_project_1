e42faa5eaa5ea3ea33b261d2b1c4e6a6cf152b49
spaghetconfet <67653932+spaghetconfet@users.noreply.github.com>
2024-09-26 01:46:19 -0400

    overhauled mostly everything
    still need to beautify with actual assets, make a menu, score, ETC

--- Assets/Scripts/BirdInput.cs
+++ Assets/Scripts/BirdInput.cs
@@ -5,23 +5,19 @@ using UnityEngine;
 public class BirdInput : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-    [SerializeField]private float jumpForce = 200;
-    // Start is called before the first frame update
+    [SerializeField]private float jumpForce = 10.0f;
+
     void Awake()
     {
+        //Searches object for Rigidbody2D and assigns the reference to rb2d.
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //If space is pressed then jump. Velocity is Vector2 so need to use Vector2.up
         if (Input.GetKeyDown(KeyCode.Space)) {
-            //increase velocity y
-            Vector2 vel = rb2d.velocity;
-            vel.y = jumpForce;
-            rb2d.velocity = vel;
-            
-
+            rb2d.velocity = Vector2.up * jumpForce; 
         }
 
     }
