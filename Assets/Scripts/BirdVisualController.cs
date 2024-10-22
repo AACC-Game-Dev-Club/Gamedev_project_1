7ee281afee102ddbe0b759d154bc8ed4b88728ba
Caleb <clawrenceharris@gmail.com>
2024-10-22 18:30:48 -0400

    bird animation implememented

---
+++ Assets/Scripts/BirdVisualController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FlappyAnimation{
+    public Sprite[] sprites;
+
+}
+public class BirdVisualController : MonoBehaviour
+{
+    
+    [SerializeField]private FlappyAnimation flappyAnimation;
+    private SpriteRenderer spriteRenderer;
+    private void Awake(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start(){
+        StartCoroutine(Animate());
+    }
+
+    private IEnumerator Animate(){
+        while(true){
+            foreach(Sprite sprite in flappyAnimation.sprites){
+            spriteRenderer.sprite = sprite;
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
+        }
+       
+    }
+    
+
+
+}
