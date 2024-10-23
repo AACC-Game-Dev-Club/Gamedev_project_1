20f2c23d2309a7a1762f070582ef3be8b0369381
Caleb <clawrenceharris@gmail.com>
2024-10-23 18:29:00 -0400

    flappy animation logic refactored and improved

--- Assets/Scripts/PlayerVisualController.cs
+++ Assets/Scripts/PlayerVisualController.cs
@@ -3,34 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class FlappyAnimation{
+public class FlappyAnimation
+{
     public Sprite[] sprites;
-
+    public float frameRate = 0.05f;
 }
+
 public class PlayerVisualController : MonoBehaviour
 {
-    
-    [SerializeField]private FlappyAnimation flappyAnimation;
+    [SerializeField] private FlappyAnimation flappyAnimation;
     private SpriteRenderer spriteRenderer;
-    private void Awake(){
+    private bool isAnimating = true;
+    private int spriteCount;
+
+    private void Awake()
+    {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteCount = flappyAnimation.sprites.Length; // Cache the sprite array length
     }
 
-    private void Start(){
+    private void Start()
+    {
         StartCoroutine(Animate());
     }
 
-    private IEnumerator Animate(){
-        while(true){
-            foreach(Sprite sprite in flappyAnimation.sprites){
-            spriteRenderer.sprite = sprite;
-            yield return new WaitForSeconds(0.1f);
+    private IEnumerator Animate()
+    {
+        int index = 0;
+        
+        while (isAnimating)
+        {
+            // Cycle through sprites
+            spriteRenderer.sprite = flappyAnimation.sprites[index];
+            index = (index + 1) % spriteCount; // Loop back to the start when reaching the end
+
+            // Wait based on frame rate
+            yield return new WaitForSeconds(flappyAnimation.frameRate);
         }
-        yield return null;
-        }
-       
     }
-    
 
+    public void StopAnimation()
+    {
+        isAnimating = false;
+    }
 
+    public void StartAnimation()
+    {
+        if (!isAnimating)
+        {
+            isAnimating = true;
+            StartCoroutine(Animate());
+        }
+    }
 }
